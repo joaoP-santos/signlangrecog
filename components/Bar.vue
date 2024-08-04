cd5be83ed5b7ef1d36362b33487e9070a32297a8
João Pedro Santos <santosjoaopedroms@gmail.com>
2024-08-03 23:35:36 -0300

    feat: added support for ASL

--- components/Bar.vue
+++ components/Bar.vue
@@ -3,10 +3,7 @@ const props = defineProps(["width"]);
 </script>
 
 <template>
-  <div class="w-full bg-gray-200 rounded-full h-2.5">
-    <div
-      class="bg-blue-600 h-2.5 rounded-full"
-      :style="{ width: `${width}%` }"
-    ></div>
+  <div class="w-full bg-gray-200 h-2.5">
+    <div class="bg-blue-600 h-2.5" :style="{ width: `${width}%` }"></div>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="flex flex-col gap-2 justify-center items-center self-center h-svh"
+  >
+    <h1 class="text-5xl font-light">
+      Let's start LIVE sign language recognizing
+    </h1>
+    <p>But first, pick your language</p>
+    <div
+      class="flex flex-row gap-8 *:border *:p-4 *:w-24 *:text-center *:border-blue-400"
+    >
+      <NuxtLink to="/libras">LIBRAS</NuxtLink>
+      <NuxtLink to="/asl">ASL</NuxtLink>
+    </div>
+  </div>
+</template>
